31515dc903c3072b2636737d125d971a0c1011f0
wahome <wahomewgitonga@gmail.com>
2024-05-18 16:58:33 +0300

    update

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
 
 def greet(name):
-    pass
+    print (f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    sum= num1 + num2
+    return sum
+
+num1 = 45
+num2 = 55
+
+
+
 
 def halve(number):
-    pass
+    return number / 2
